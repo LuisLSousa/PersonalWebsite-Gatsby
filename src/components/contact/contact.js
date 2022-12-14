ab7812431794531cc4252a35bf7caa6e19e2f845
Luís Lampreia de Sousa <luis.lampreia@olx.com>
2022-12-14 20:49:04 +0000

    fixing contact

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,126 +1,7 @@
 import React, { useRef, useState } from "react";
-import styled from "styled-components";
 import Heading from "../heading/heading";
 import Button from "../button/button"
-
-// ###### CSS #########
-const Wrapper = styled.section`
-  border-top: 2px solid rgba(255, 255, 255, 0.8);
-  height: 90vh;
-  min-height: 600px;
-
-  /* mobile */
-  @media only screen and (max-height: 900px) {
-    height: 85vh;
-  }
-  /* vertical ultrawide monitors */
-  @media only screen and (min-height: 1400px) {
-    height: 95vh;
-  }
-`;
-
-const StyledDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  form {
-    margin-bottom: 0;
-  }
-
-  .field {
-    margin-left: 1rem;
-    background: transparent;
-    color: rgb(232, 232, 232);
-    border: 2px solid rgb(75, 85, 99);
-    margin-bottom: 1vh;
-    padding: 0.5rem 1rem;
-    font-size: 1.125rem;
-    line-height: 1.75rem;
-    transition-duration: 0.3s;
-    margin-bottom: 3vh;
-  }
-
-  .field:focus {
-    border: 2px solid #effffa;
-    outline: 2px solid transparent;
-    outline-offset: 2px;
-  }
-
-  .name {
-    min-width: 240px;
-    width: 20vw;
-  }
-
-  .email {
-    min-width: 300px;
-    width: 25vw;
-  }
-
-  .message {
-    min-width: 420px;
-    width: 35vw;
-  }
-
-  h2 {
-    font-size: 1.25rem;
-    font-weight: 400;
-    margin-left: 1rem;
-    margin-bottom: 1vh;
-  }
-
-  .error {
-    border-color: rgb(239, 68, 68);
-    animation: shake 1s;
-  }
-
-  @keyframes shake {
-    10%,
-    90% {
-      transform: translate3d(-1px, 0, 0);
-    }
-
-    20%,
-    80% {
-      transform: translate3d(2px, 0, 0);
-    }
-
-    30%,
-    50%,
-    70% {
-      transform: translate3d(-4px, 0, 0);
-    }
-
-    40%,
-    60% {
-      transform: translate3d(4px, 0, 0);
-    }
-  }
-
-  @media (max-width: 562px) {
-    .field {
-      margin-left: 0;
-      font-size: 1rem;
-      line-height: 1.5rem;
-    }
-    h2 {
-      margin-left: 0;
-    }
-    .name {
-      min-width: 180px;
-      width: 60vw;
-    }
-    .email {
-      min-width: 240px;
-      width: 70vw;
-    }
-    .message {
-      min-width: 240px;
-      width: 80vw;
-    }
-  }
-`;
-
-// ####################
+import "./contact.css"
 
 function encode(data) {
   return Object.keys(data)
@@ -192,9 +73,8 @@ const Contact = () => {
   };
 
   return (
-    <Wrapper id="contact">
+    <div id="contact" className="wrapper">
       <Heading title="Contact" />
-      <StyledDiv>
         <form
           method="post"
           name="contact"
@@ -242,8 +122,7 @@ const Contact = () => {
           </label>
           <Button title={buttonText} />
         </form>
-      </StyledDiv>
-    </Wrapper>
+    </div>
   );
 };
 
