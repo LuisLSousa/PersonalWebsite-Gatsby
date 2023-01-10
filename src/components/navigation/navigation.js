eb794827d1e79d0b95e574a24ae9dcf8b5edbce7
LuisLSousa <luis.srl.sousa@gmail.com>
2023-01-10 21:07:06 +0000

    fixing navigation

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-scroll";
+import { navList } from '../../data/navigation';
 
 import "./navigation.css";
 
 const Navigation = () => {
-  const [navigation, setNavigation] = useState(false);
   const [openMenu, toggleMenu] = useState(false);
-  const menuRef = useRef(null);
-  const buttonRef = useRef(null);
+  const navRef = useRef(null);
 
   useEffect(() => {
     /* Close the menu when the user clicks outside of it */
     const closeMenu = (event) => {
-      if (menuRef.current && menuRef.current.contains(event.target)) {
+      if (navRef.current && navRef.current.contains(event.target)) {
         return;
       }
       toggleMenu(false);
@@ -21,148 +20,58 @@ const Navigation = () => {
     return () => document.removeEventListener("mousedown", closeMenu);
   }, []);
 
-  // change navbar background on scroll
-  const changeBackground = () => {
-    if (window.scrollY > 50) {
-      setNavigation(true);
-    } else {
-      setNavigation(false);
-    }
-  };
+  // // change navbar background on scroll
+  // const changeBackground = () => {
+  //   if (window.scrollY > 50) {
+  //     setNavigation(true);
+  //   } else {
+  //     setNavigation(false);
+  //   }
+  // };
 
-  useEffect(() => {
-    // initiate the event handler
-    window.addEventListener("scroll", changeBackground);
-    // this will clean up the event every time the component is re-rendered
-    return function cleanup() {
-      window.removeEventListener("scroll", changeBackground);
-    };
-  });
+  // useEffect(() => {
+  //   // initiate the event handler
+  //   window.addEventListener("scroll", changeBackground);
+  //   // this will clean up the event every time the component is re-rendered
+  //   return function cleanup() {
+  //     window.removeEventListener("scroll", changeBackground);
+  //   };
+  // });
 
   return (
-    <nav ref={menuRef}>
-        <div className="mobileMenu">
-          <Link
-            className="logo"
-            activeClass="selected"
-            to="home"
-            spy={true}
-            smooth={true}
-            offset={-55}
-            duration={600}
-            isDynamic={true}
-          >
-            Luís Sousa
-          </Link>
-            <button onClick={() => toggleMenu(!openMenu)}
-            className="menuButton"
-            ref={buttonRef}
-            aria-label="Navigation">
-              <div className={openMenu ? "menuLayer open" : "menuLayer"}/>
-              <div className={openMenu ? "menuLayer open" : "menuLayer"}/>
-              <div className={openMenu ? "menuLayer open" : "menuLayer"}/>
-            </button>
-        </div>
-      <ul className={openMenu ? "menu open" : "menu"}
-        openMenu={openMenu}>
-        <li className="navItem">
-          <Link
-            className="link"
-            activeClass="selected"
-            to="home"
-            spy={true}
-            smooth={true}
-            offset={-55}
-            duration={600}
-            isDynamic={true}
-          >
-            Home
-          </Link>
-        </li>
-        <li className="navItem">
-          <Link
-            className="link"
-            activeClass="selected"
-            to="about"
-            spy={true}
-            smooth={true}
-            offset={-55}
-            duration={600}
-            isDynamic={true}
-          >
-            About Me
-          </Link>
-        </li>
-        <li className="navItem">
-          <Link
-            className="link"
-            activeClass="selected"
-            to="experience"
-            spy={true}
-            smooth={true}
-            offset={-55}
-            duration={600}
-            isDynamic={true}
-          >
-            Experience
-          </Link>
-        </li>
-        <li className="navItem">
-          <Link
-            className="link"
-            activeClass="selected"
-            to="education"
-            spy={true}
-            smooth={true}
-            offset={-55}
-            duration={600}
-            isDynamic={true}
-          >
-            Education
-          </Link>
-        </li>
-        <li className="navItem">
-          <Link
-            className="link"
-            activeClass="selected"
-            to="projects"
-            spy={true}
-            smooth={true}
-            offset={-55}
-            duration={600}
-            isDynamic={true}
-          >
-            Projects
-          </Link>
-        </li>
-        <li className="navItem">
-          <Link
-            className="link"
-            activeClass="selected"
-            to="skills"
-            spy={true}
-            smooth={true}
-            offset={-55}
-            duration={600}
-            isDynamic={true}
-          >
-            Skills
-          </Link>
-        </li>
-        <li className="navItem">
-          <Link
-            className="link"
-            activeClass="selected"
-            to="contact"
-            spy={true}
-            smooth={true}
-            offset={-55}
-            duration={600}
-            isDynamic={true}
-          >
-            Contact
-          </Link>
-        </li>
+    <nav ref={navRef}>
+      <div className="mobileMenu">
+        <Link
+          className="logo"
+          activeClass="selected"
+          to="home"
+          spy={true}
+          smooth={true}
+          offset={-55}
+          duration={600}
+          isDynamic={true}
+        >
+          LL
+        </Link>
+        <button onClick={() => toggleMenu(!openMenu)}
+          className="menuButton"
+          aria-label="Navigation">
+          <div className={openMenu ? "menuLayer open" : "menuLayer"} />
+          <div className={openMenu ? "menuLayer open" : "menuLayer"} />
+          <div className={openMenu ? "menuLayer open" : "menuLayer"} />
+        </button>
+      </div>
+      <ul className={openMenu ? "navItems open" : "navItems"}>
+        {navList.map(({ sectionId, icon, name }) => {
+          return (
+            <li>
+              <Link className="navItem" activeClass="selected" to={sectionId} spy={true} smooth={true} offset={-55} duration={600} isDynamic={true}>
+                <span className='icon'>{icon}</span>
+                <span className='text'>{name}</span>
+              </Link>
+            </li>
+          )
+        })}
       </ul>
     </nav>
   );
