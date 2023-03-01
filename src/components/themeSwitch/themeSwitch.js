6b291defd1c5ac3e19a4c8c2e1edabdf0ad9f469
LuisLSousa <luis.srl.sousa@gmail.com>
2023-03-01 08:57:38 +0000

    saving theme in local storage

--- src/components/themeSwitch/themeSwitch.js
+++ src/components/themeSwitch/themeSwitch.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useContext}  from 'react';
+import React, { useEffect, useContext } from 'react';
 import { ThemeContext, DARK_THEME } from "../../context/theme";
 import { HiMoon, HiSun } from 'react-icons/hi';
 
@@ -16,13 +16,14 @@ const ThemeSwitch = () => {
 
     return (
         <label className="themeSwitch" aria-label="toggle theme">
-            <input type="checkbox" onClick={() => { toggleTheme(); }} />
+            <input type="checkbox" onClick={() => { toggleTheme() }} defaultChecked={theme === DARK_THEME} />
             <span className="slider"></span>
             <div className="themeOptions">
                 <HiMoon className="moon" />
                 <HiSun className="sun" />
             </div>
-        </label>);
+        </label>
+    );
 }
 
 export default ThemeSwitch;
--- src/context/theme.js
+++ src/context/theme.js
@@ -3,17 +3,24 @@ import React, { createContext, useState } from 'react';
 export const DARK_THEME = 'dark';
 export const LIGHT_THEME = 'light';
 
-
 export const ThemeContext = createContext({
     theme: DARK_THEME,
     toggleTheme: () => { }
 });
 
+const getThemeFromStorage = () => localStorage.getItem('theme');
+
+const setThemeInStorage = (theme) => {
+    localStorage.setItem('theme', theme)
+}
+
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState(DARK_THEME);
+
+    const [theme, setTheme] = useState(getThemeFromStorage() || DARK_THEME);
 
     const toggleTheme = () => {
-        setTheme(theme ===  DARK_THEME ? LIGHT_THEME : DARK_THEME);
+        setTheme(theme === DARK_THEME ? LIGHT_THEME : DARK_THEME);
+        setThemeInStorage(theme === DARK_THEME ? LIGHT_THEME : DARK_THEME);
     };
 
     return (
